5581b9fca1a76da461f9b5da247d755a7078e6ec
rrafaelramos <rrafaelramos2@gmail.com>
2021-01-20 20:36:13 -0300

    retornando nome do Juiz

--- php7/index.php
+++ php7/index.php
@@ -18,11 +18,44 @@
     // passa para utf-8
 //    $resultado = utf8_decode($busca);
 
-
+    $i = 0;
+    $position_juiz = [];
+    $position_escrivao = [];
+    $parte = '';
     while ($publicacao = $busca->fetch_array()){
         echo '<hr/>';
+
+        $position_juiz[$i] = strpos($publicacao["ra_conteudo"], "JUIZ(A)");
+        $position_escrivao[$i] = strpos($publicacao["ra_conteudo"], "ESCRIV");
+        $quantidade_para_tras = $position_juiz[$i] - $position_escrivao[$i];
+
+        $tamanho = strlen($publicacao["ra_conteudo"]);
+        $texto = ($tamanho-$position_escrivao[$i]);
+
+        $nome_juiz[$i] = substr($publicacao["ra_conteudo"], $position_juiz[$i], -$texto);
+
+        $nome = "Linha de codigo";
+        $parte = substr($nome, 6, -7);
+
         echo utf8_encode($publicacao["ra_conteudo"].'<br/><br/><br/><br/><br/><br/><br/><br/>');
+        $i++;
     }
+    echo '<br/><br/><br/><br/><br/>';
+//    foreach ($position_juiz as $juiz){
+//        echo $juiz.'<br/>';
+//    }
+//
+//    echo '<br/><br/><br/><br/><br/><br/><br/><br/>';
+//    foreach ($position_escrivao as $escrivao){
+//        echo $escrivao.'<br/>';
+//    }
+
+foreach ($nome_juiz as $juiz){
+    echo utf8_encode($juiz).'<br/>';
+}
+
+echo $parte;
+
 
 
 
